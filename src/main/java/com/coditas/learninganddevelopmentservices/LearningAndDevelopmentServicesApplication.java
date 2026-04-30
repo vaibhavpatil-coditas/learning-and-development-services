8e5edb51ef7b48aaaf7d2b3bc5eb572fa5d2c37a
vaibhavpatil-coditas <vaibhav.patil@coditas.com>
2026-04-30 15:01:18 +0530

    Initial Commit

---
+++ src/main/java/com/coditas/learninganddevelopmentservices/LearningAndDevelopmentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.coditas.learninganddevelopmentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAndDevelopmentServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningAndDevelopmentServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coditas/learninganddevelopmentservices/LearningAndDevelopmentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coditas.learninganddevelopmentservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAndDevelopmentServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
